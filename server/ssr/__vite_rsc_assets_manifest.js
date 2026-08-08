84bc4c338cd55541b5e88de34e9caa17bad71d0b
dr-mritunjaysp <dr-mritunjaysp@users.noreply.github.com>
2026-08-08 09:10:03 +0000

    deploy: e9d9a59de68080299262ded600a58015ffefe8a8

--- server/ssr/__vite_rsc_assets_manifest.js
+++ server/ssr/__vite_rsc_assets_manifest.js
@@ -1,11 +1,11 @@
 export default {
-  "bootstrapScriptContent": "import(\"/assets/index-DJORV7Aw.js\")",
+  "bootstrapScriptContent": "import(\"/assets/index-C0B1gvse.js\")",
   "clientReferenceDeps": {
     "ab59f52a2ee1": {
       "js": [
-        "/assets/PortfolioApp-CILH3VFT.js",
+        "/assets/PortfolioApp-Bld5PyIN.js",
         "/assets/rolldown-runtime-S-ySWqyJ.js",
-        "/assets/index-DJORV7Aw.js",
+        "/assets/index-C0B1gvse.js",
         "/assets/framework-CXnKph_e.js",
         "/assets/query-D8Wk3mvj.js"
       ],
@@ -13,7 +13,7 @@ export default {
     },
     "593f344dc510": {
       "js": [
-        "/assets/index-DJORV7Aw.js",
+        "/assets/index-C0B1gvse.js",
         "/assets/rolldown-runtime-S-ySWqyJ.js",
         "/assets/framework-CXnKph_e.js"
       ],
@@ -21,16 +21,16 @@ export default {
     },
     "15c18cfaeeff": {
       "js": [
-        "/assets/layout-segment-context-D8MaAdJ8.js",
+        "/assets/layout-segment-context-BGJxxliz.js",
         "/assets/rolldown-runtime-S-ySWqyJ.js",
-        "/assets/index-DJORV7Aw.js",
+        "/assets/index-C0B1gvse.js",
         "/assets/framework-CXnKph_e.js"
       ],
       "css": []
     },
     "8c0f216c4604": {
       "js": [
-        "/assets/index-DJORV7Aw.js",
+        "/assets/index-C0B1gvse.js",
         "/assets/rolldown-runtime-S-ySWqyJ.js",
         "/assets/framework-CXnKph_e.js"
       ],
--- server/ssr/index.js
+++ server/ssr/index.js
@@ -11669,7 +11669,7 @@ var client_references_default = {
 		};
 	},
 	"ab59f52a2ee1": async () => {
-		const m = await import("./assets/PortfolioApp-Cq-CCz0w.js");
+		const m = await import("./assets/PortfolioApp-BGRszORp.js");
 		return { get "PortfolioApp"() {
 			return m["PortfolioApp"];
 		} };
