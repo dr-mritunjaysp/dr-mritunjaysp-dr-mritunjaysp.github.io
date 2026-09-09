985a92fdebbf09e1dc073f48bd2a95d394328705
Mritunjay Shall Peelam <mritunjay.iete@gmail.com>
2026-09-09 22:56:22 +0530

    Fix clipped Finger Counter digits

--- app/finger-counter/FingerCounterPage.tsx
+++ app/finger-counter/FingerCounterPage.tsx
@@ -4,7 +4,7 @@ export function FingerCounterPage() {
       <div className="finger-counter-frame-shell">
         <iframe
           className="finger-counter-frame"
-          src="/finger-counter-app/index.html?v=20260909-firework-ui"
+          src="/finger-counter-app/index.html?v=20260909-firework-fit"
           title="Animated finger counter"
           allow="camera; fullscreen"
           allowFullScreen
